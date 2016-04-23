d99a93fa39cb2749e8d9ca39cfc3b17324343a81
msquarednet <msquarednet@gmail.com>
2016-04-23 11:28:07 -0400

    dev-watch

--- sillylittlewebsite/index.js
+++ sillylittlewebsite/index.js
@@ -1,13 +1,35 @@
 var app = require('koa')();
 
+var html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Timestamp Test Page!</title>
+    <script src="//localhost:9091"></script> 
+  </head>
+  <body>
+    <p>Page rendered at: <br><span id="timestamp"></span></p>
+  </body>
+  <script>
+    document.getElementById('timestamp').innerHTML = new Date();
+  </script>
+</html>
+`;  //very hacky
 
 app.use(function *(){
-  this.body = 'Koa says Hi!';
+  if (this.request.path === '/client') {
+    this.body = html; return;
+  }
+  this.body = 'Koa says Hi';
 })
 
 // var port = process.env.PORT || process.argv[2] || 3000;
 var port = process.env.PORT || 3000;
-app.listen(port);
+//app.listen(port);
+if(!module.parent) {  // if NOT testing...
+    app.listen(port);
+}
 
 console.log('Application Started. PORT:'+port);
 
